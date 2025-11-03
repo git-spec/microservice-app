fcb400bb5f2c6efc74f43511d3369a4861306d54
git-spec <inkyfischer@gmail.com>
2025-11-03 17:01:57 +0100

    feat: Created enum Group.

---
+++ service-product/src/main/java/com/example/models/Group.java
@@ -0,0 +1,19 @@
+package com.example.models;
+
+
+public enum Group {
+  TABLE("Table"),
+  STORAGE("Storage"),
+  SEATING("Seating"),
+  BED("Bed");
+
+  private final String value;
+
+  Group(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+}
