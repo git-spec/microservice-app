aa9529aeb4da78d94a39d0271e98249a005b783e
git-spec <inkyfischer@gmail.com>
2025-11-03 23:30:26 +0100

    feat: Created class Product of document products.

---
+++ service-product/src/main/java/com/example/models/Product.java
@@ -0,0 +1,36 @@
+package com.example.models;
+
+import java.math.BigDecimal;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+
+@Document("products")
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+public class Product {
+    @Id
+    private final String id;
+    private final String number;
+    private final String name;
+    private final String manufacturer;
+    private final Category category;
+    private final Group group;
+    private final Family family;
+    private final ProductFeatures features;
+    private final String info;
+    private final String description;
+    private final Images images;
+    private final BigDecimal price;
+    private final Currency currency;
+    private final int amount;
+
+}
