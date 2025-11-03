b3a5b38ce93211ceaef59f83c69be7af4cb02329
git-spec <inkyfischer@gmail.com>
2025-11-03 17:01:37 +0100

    feat: Created enum Family.

---
+++ service-product/src/main/java/com/example/models/Family.java
@@ -0,0 +1,19 @@
+package com.example.models;
+
+import lombok.Getter;
+
+
+@Getter
+public enum Family {
+  CHAIR("Chair"),
+  STOOL( "Stool"),
+  SOFA("Sofa"),
+  COUCH("Couch"),
+  TABLE("Table");
+
+  private final String value;
+
+  Family(String value) {
+    this.value = value;
+  }
+}
