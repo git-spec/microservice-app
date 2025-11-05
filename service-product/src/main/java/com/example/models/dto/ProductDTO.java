7b88fba8a019f1c00dc566fd16119992c0a3ddc7
git-spec <inkyfischer@gmail.com>
2025-11-05 15:16:21 +0100

    feat: Created model ProductDTO.

---
+++ service-product/src/main/java/com/example/models/dto/ProductDTO.java
@@ -0,0 +1,35 @@
+package com.example.models.dto;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import com.example.models.Category;
+import com.example.models.Group;
+import com.example.models.Family;
+import com.example.models.ProductFeatures;
+import com.example.models.Images;
+import com.example.models.Currency;
+
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+public class ProductDTO {
+    private final String number;
+    private final String name;
+    private final String manufacturer;
+    private final Category category;
+    private final Group group;
+    private final Family family;
+    private final ProductFeatures features;
+    private final String info;
+    private final String description;
+    private final Images images;
+    private final BigDecimal price;
+    private final Currency currency;
+}
