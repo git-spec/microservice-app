232b763cb5ea8a7fdf5833234c3c4daba6d9ddad
git-spec <inkyfischer@gmail.com>
2025-11-18 18:07:32 +0100

    style: Renamed package models.

--- service-product/src/main/java/com/example/dto/ProductDTO.java
+++ service-product/src/main/java/com/example/dto/ProductDTO.java
@@ -7,12 +7,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import com.example.models.Category;
-import com.example.models.Group;
-import com.example.models.Family;
-import com.example.models.ProductFeatures;
-import com.example.models.Images;
-import com.example.models.Currency;
+import com.example.model.Category;
+import com.example.model.Group;
+import com.example.model.Family;
+import com.example.model.ProductFeatures;
+import com.example.model.Images;
+import com.example.model.Currency;
 
 
 @Builder
