66add129ab03c0da9517fd3902c6578061f6d8fe
git-spec <inkyfischer@gmail.com>
2026-01-26 09:50:56 +0100

    feat: Created RepositoryProduct.

---
+++ service-product/src/main/java/com/example/repository/RepositoryProduct.java
@@ -0,0 +1,12 @@
+package com.example.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.model.Product;
+
+
+@Repository
+public interface RepositoryProduct extends MongoRepository<Product, String> {
+
+}
